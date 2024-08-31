6a58bfa4ecfac0cd57c948e997d96c76713f90ad
Melal <Melal.1@hotmail.com>
2024-09-01 02:51:48 +0300

    Update curl.sh

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
+# This script is meant to reach by bash <(curl -L tunyurl.com/suckst)
 cd ~
 echo "Cloning the Suckless config Project"
 git clone https://github.com/Melal1/suckless.git
-
 echo "Executing Setup Script"
-
 cd $HOME/suckless/scripts/
-
 exec ./setup.sh
