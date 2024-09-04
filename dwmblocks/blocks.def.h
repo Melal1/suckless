9336638f6106c72a0e3d4680a118a6dea8d832c8
melal <melal.1@hotmail.com>
2024-09-04 23:28:27 +0300

    Added volume buttons and added newmouse buttons and dwmblocks scripts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,12 +1,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "mem.sh",	20,		0},
-	{"^c#B97E11^TEST", "cpu.sh",	5,		5},
+	{"", "volume",	 0,		16},
+	{"", "mem",	 20,		6},
+	{"", "storage",	 60,		2},
+	{" ", "cpu",	 3,			5},
+	{" ", "clock", 45,		4},
+	{"", "openurl", 0, 3},
 
-
-	{"", "clock.sh",					45,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "";
 static unsigned int delimLen = 5;
