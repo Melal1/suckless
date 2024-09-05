72ea9ec91a571ebc29bb76eca237cf768c7cec3f
melal <melal.1@hotmail.com>
2024-09-05 10:44:18 +0300

    fixed 10-monitor.conf

--- scripts/optional.sh
+++ scripts/optional.sh
@@ -22,19 +22,22 @@ Section "Monitor"
     Identifier   "DisplayPort-0"
         Option  "PreferredMode"  "2560x1440_180"
         Option  "RightOf" "HDMI-A-0"
+	Modeline "2560x1440_180"  706.00  2560 2568 2600 2670  1440 1443 1448 1470 +hsync -vsync
+	Option 	"Primary" "true"
 EndSection
 
 Section "Monitor"
     Identifier   "HDMI-A-0"
-        Option  "PreferredMode"  "1080x1920_75"
         Option  "LeftOF" "DisplayPort-0"
-	Option	"Rotate" "right"
+	Option	"Rotate" "left"
+	Modeline "1920x1080_75"  174.50  1920 1968 2000 2080  1080 1083 1088 1119 +hsync -vsync
+        Option  "PreferredMode"  "1920x1080_75"
 EndSection
 EOF
 
 cat << 'EOF1' > "$HOME/20-amdgpu.conf" 
 Section "Device"
-        Identifier      "AMD"
+        Identifier      "AMD Graphics"
         Driver          "amdgpu"
         Option          "TearFree" "true"
 EndSection
